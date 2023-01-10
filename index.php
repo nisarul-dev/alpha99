fcbd9f7798de4f9569fdd1673c45dc68cb935380
Nisarul Amin Naim <65971192+nisarul-media@users.noreply.github.com>
2023-01-10 16:55:16 +0600

    Bloginfo -> name and description updated.

--- index.php
+++ index.php
@@ -64,8 +64,8 @@
     <div class="container">
         <div class="row">
             <div class="col-md-12">
-                <h3 class="tagline">No act of kindness, no matter how small, is ever wasted</h3>
-                <h1 class="align-self-center display-1 text-center heading">Hello Beautiful</h1>
+                <h3 class="tagline"><?php bloginfo( 'description' ); ?></h3>
+                <h1 class="align-self-center display-1 text-center heading"><?php bloginfo( 'name' ); ?></h1>
             </div>
         </div>
     </div>
