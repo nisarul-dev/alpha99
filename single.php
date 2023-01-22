484a64142526db5d6fc9023e671ff93d2c530245
Nisarul Amin Naim <65971192+nisarul-media@users.noreply.github.com>
2023-01-22 14:35:44 +0600

    Comment Section

--- single.php
+++ single.php
@@ -102,6 +102,12 @@
 							</p>
 							<?php the_content(); ?>
 						</div>
+
+						<?php if ( comments_open() ) : ?>
+						<div class="col-md-10 offset-md-1 my-5">
+							<?php comments_template(); ?>
+						</div>
+						<?php endif; ?>
 					</div>
 
 				</div>
